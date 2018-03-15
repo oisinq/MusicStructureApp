b7ef9ffd14305e23cec167567217f40b1ca403f6
Oisin Quinn <oisin1001@gmail.com>
2018-03-15 23:16:10 +0000

    Added title bar to Now Playing activity

--- app/src/main/java/io/oisin/musicstructureapp/MusicActivity.java
+++ app/src/main/java/io/oisin/musicstructureapp/MusicActivity.java
@@ -1,14 +1,14 @@
 package io.oisin.musicstructureapp;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class MusicActivity extends Activity {
+public class MusicActivity extends AppCompatActivity {
 
     Episode episode;
 
